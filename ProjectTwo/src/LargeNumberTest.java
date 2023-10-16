b0db32a3af8c413a680bc07e17462a926d968501
Samwell <dizzygokartman@gmail.com>
2023-10-16 19:29:53 -0400

    Worked a bit more, output is a bit messed up rn. I think the way I'm storing the intnodes is messed up so I'm gonna keep working on it.

--- ProjectTwo/src/LargeNumberTest.java
+++ ProjectTwo/src/LargeNumberTest.java
@@ -1,16 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class LargeNumberTest{
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-        String input = "123456";
+        String input;
 
         System.out.print("Please input the first number\n>");
-        //input = kb.getline();
+        input = kb.nextLine();
         UnboundedInt num1 = new UnboundedInt(input);
         System.out.print("Please input the second number\n>");
-        //input = kb.getline();
+        input = kb.nextLine();
         UnboundedInt num2 = new UnboundedInt(input);
+
+        System.out.println(num1.toString());
 
     }
 }
--- ProjectTwo/src/UnboundedInt.java
+++ ProjectTwo/src/UnboundedInt.java
@@ -15,22 +15,34 @@ public class UnboundedInt {
 
     //This constructor will take a string of digits(no commas) and turn it into an UnboundedInt object (MUST BE STRING INPUT)
     public UnboundedInt(String input){
-        manyNodes = input.length();
         String temp;
-        int tempValue, count;
+        int tempValue;
+        int count;
+        int remainder;
+        boolean flag = true;
 
-        temp = input.substring(0, 2);
+        temp = input.substring(0, 3);
         tempValue = Integer.parseInt(temp);
 
-        front(tempValue);
-
+        front = new IntNode(tempValue, null);
         cursor = front;
 
-        for(int x = 3; x < manyNodes; x += 3){
-            temp = input.substring(x, (x + 3));
+        remainder = input.length() % 3;
+
+        if(remainder == 1){
+            input = "00" + input;
+        }else if(remainder == 2){
+            input = "0" + input;
+        }
+
+        manyNodes = input.length() / 3;
+
+        start();
+        for(count = 1; count < manyNodes; count++){
+            temp = input.substring((count * 3), ((count * 3) + 3));
             tempValue = Integer.parseInt(temp);
             cursor.addNodeAfter(tempValue);
-            cursor = cursor.listPosition(front, count);
+            advance();
         }
 
         back = cursor;
@@ -46,41 +58,51 @@ public class UnboundedInt {
             }else{
                 cursor = cursor.getLink();
             }
-        }catch IllegalStateException();
+        }catch(IllegalStateException e){
+            return;
+        }
     }
 
     //a method that returns the integer value of the Node that is pointed to by the cursor.
     //Throws an IllegalStateException if the cursor is not pointing to a Node
     public int getNodeValue(){
         try{
-            if(cursor.getData() == null){
+            if(cursor == null){
                 throw new IllegalStateException();
             }else{
                 return cursor.getData();
             }
-        }catch IllegalStateException();
+        }catch(IllegalStateException e){
+            return -1;
+        }
     }
 
     //Same as toString but no commas in string.
     public String toStringNoCommas(){
-
+        return "";
     }
 
     //Creates a string of all elements in order separated by commas,
     //making sure leading zeros are added when needed.  (i.e.  12,005,016 or  34,000 )
     //Throw an IllegalStateException if the sequence is empty
+
     public String toString(){
         String output = "";
+        int length = IntNode.listLength(front);
 
         try{
-            if(front.getData() == null){
+            if(front == null){
                 throw new IllegalStateException();
             }else{
                 start();
-                output += cursor.getData() + ",";
-                advance();
+                for(int x = 0; x < manyNodes; x++){
+                    output += cursor.getData() + ",";
+                    advance();
+                }
             }
-        }catch IllegalStateException();
+        }catch(IllegalStateException e){
+            return "";
+        }
 
         return output;
     }
