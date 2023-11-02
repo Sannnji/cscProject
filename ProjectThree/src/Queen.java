654278b7ce226a09d999aaf870b6b448aa259799
Samwell <dizzygokartman@gmail.com>
2023-11-02 15:06:48 -0400

    JavaDoc, cleaned some code up, comments, misc stuff

--- ProjectThree/src/Queen.java
+++ ProjectThree/src/Queen.java
@@ -1,19 +1,56 @@
+/******************************************************************************
+ * A Queen object represents queen on a chess board. The object possesses
+ * coordinates to indicate the position of the queen on the board.
+ *
+ * @author James Ji, Samuel Acquaviva
+ *
+ ******************************************************************************/
 public class Queen {
     private int row;
     private int column;
 
+    /**
+     * Accessor method to get the row that the queen is in.
+     * @return
+     * The integer value of the row that the queen is positioned in.
+     **/
     public int getRow() {
         return row;
     }
+
+    /**
+     * Accessor method to get the column that the queen is in.
+     * @return
+     * The integer value of the column that the queen is positioned in.
+     **/
     public int getCol() {
         return column;
     }
 
-    Queen (int row, int column) {
+    /**
+     * Constructor for the Queen object.
+     * @param row
+     * The row that the Queen object will be placed in.
+     * @param column
+     * The column that the Queen object will be place in.
+     * @precondition
+     * The row and column values don't exceed the size of the
+     * current board configuration.
+     **/
+     Queen (int row, int column) {
         this.row = row;
         this.column = column;
     }
 
+    /**
+     * Checks to see if the passed in Queen object could take the
+     * referenced Queen object based on the positions of both objects.
+     * @param - queen
+     * A Queen object used to compare with the Queen that the method is called on.
+     * @return
+     * Returns True if there is a conflict between both Queen objects, and returns
+     * false if there isn't a conflict.
+     **/
     public boolean conflict(Queen queen) {
         boolean isConflicted = false;
         double slope = (queen.row - row) != 0 ? (double)(queen.column - column) / (queen.row - row) : 0.0;
@@ -31,6 +68,13 @@ public class Queen {
         return isConflicted;
     }
 
+    /**
+     * Accessor method that returns the row and column values of
+     * the Queen object that the method is called on.
+     * @return
+     * Returns a presentable string containing the row and column
+     * values of the referenced Queen object, to be printed to the user.
+     **/
     public String toString() {
         return "(row = " + row + ", column = " + column + ")";
     }
--- ProjectThree/src/QueenSimulation.java
+++ ProjectThree/src/QueenSimulation.java
@@ -1,18 +1,21 @@
+//James Ji, Samuel Acquaviva
 import java.util.Scanner;
 
 public class QueenSimulation {
-    // constant for the starting position for row and column
+    // Constant that holds the starting position for the row and column (1, 1)
     static final int STARTING_POS = 1;
+    // Static counter that holds the number of calculated solutions
+    // Sets the row and column values to the starting position
     static int solutionCount = 0, row = STARTING_POS, column = STARTING_POS;
     static boolean allSolutionsFound = false, isConflicted = false;
     static String solutionSet = "";
     static Scanner usrInput = new Scanner(System.in);
     static LinkedStack<Queen> solutionStack = new LinkedStack<Queen>();
 
-    // backtrack pop method
-    // pops a queen in the stack when the queen being placed has no available spots on the row
-    // and set the popped queen to current queen with + 1 to column
-    // if the queen being popped is
+    // Backtracking pop method:
+    // This pops a queen in the stack when the queen being placed has no
+    // available spots on the row. Then sets the popped queen equal to the
+    // current queen and increments the column of the popped Queen by 1.
     public static void popQueenAtColEnd(int boardSize) {
         if (solutionStack.peek().getCol() + 1 > boardSize) {
             solutionStack.pop();
@@ -22,6 +25,8 @@ public class QueenSimulation {
         solutionStack.pop();
     }
 
+    // Pushes the passed in Queen object to the stack,
+    // and outputs the position of the Queen to the user.
     public static void pushQueen(Queen queen) {
         solutionStack.push(queen);
         System.out.println("Queen placed at: " + queen.toString());
@@ -31,7 +36,8 @@ public class QueenSimulation {
     }
 
     public static void main(String[] args) {
-        // intro prompt
+        // Intro prompt for the user
+        // Gets the desired board size
         System.out.println("Welcome to the N-Queens Solver Simulation!");
         System.out.print("What size board would you like to use?: ");
         int boardSize = usrInput.nextInt();
@@ -39,11 +45,13 @@ public class QueenSimulation {
         System.out.println("Sounds good, the simulation will now begin populating a " + boardSize + " x " + boardSize + " size board");
         System.out.println();
 
+        // Main program loop
         while (!allSolutionsFound) {
+            // Creates a new Queen object and prints the position of the queen
             Queen queen = new Queen(row, column);
             System.out.println("Location of the Queen being placed: " + queen.toString());
 
-            // check for conflicts, break from loop if one is found
+            // Checks for conflicts and breaks from the loop if one is found.
             for (int i = 0; i < solutionStack.size(); i++) {
                 if (queen.conflict((Queen) solutionStack.itemAt(i))) {
                     isConflicted = true;
@@ -53,32 +61,35 @@ public class QueenSimulation {
                 if (isConflicted) break;
             }
 
-            // backtrack
-            // if column is equal to board size and is conflicted
-            // otherwise increment column
+            // Backtracks if the column is equal to the board size AND is conflicted,
+            // otherwise increments the column.
             if (column == boardSize && isConflicted) {
-                // this is the exiting loop
-                // if we have to backtrack to (1,5) then that means we've discovered all the possible solution
+                // If we have to backtrack to (1, Max Value) then we've discovered
+                // all the possible solutions.
                 if (solutionStack.peek().getRow() == 1 && solutionStack.peek().getCol() == boardSize) {
                     allSolutionsFound = true;
                 } else {
-                    // backtrack
+                    // Otherwise, backtrack
                     popQueenAtColEnd(boardSize);
                 }
             } else {
                 column++;
             }
 
-            // push queen if no conflicts were found
+            // Push the queen if no conflicts were found
             if (!isConflicted) pushQueen(queen);
 
-            // reset isConflicted
+            // Reset isConflicted boolean
             isConflicted = false;
-            // a solution set is found when the number of queens in the solution stack is equal to the number of rows
+
+            // A solution set is found when the number of queens in the
+            // solution stack is equal to the number of rows
             if (solutionStack.size() == boardSize) {
+                // Increments the counter and prints solution coordinates
+                // from top to bottom (according to the board)
                 solutionCount++;
-                // print solution coordinates from top to bottom (according to board)
                 solutionSet += "Solution #" + solutionCount + ": ";
+
                 for (int i = solutionStack.size() - 1; i >= 0; i--) {
                     if (i != solutionStack.size() - 1) {
                         solutionSet += ", " + solutionStack.itemAt(i).toString();
@@ -92,6 +103,7 @@ public class QueenSimulation {
 
         }
 
+        // Outputs the solution set and the total number of solutions to the user
         System.out.println(solutionSet);
         System.out.println("The total number of solutions for a " + boardSize + " x " + boardSize + " is " + solutionCount);
     }
