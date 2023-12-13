fc18665aa62f87713091cad109e840ed214b20b8
James Ji <57771801+Sannnji@users.noreply.github.com>
2023-12-12 19:14:37 -0500

    minor changes in HashTesting

--- ProjectFive/src/HashTesting.java
+++ ProjectFive/src/HashTesting.java
@@ -19,7 +19,7 @@ public class HashTesting {
         Integer key;
         String name;
         int count = 0;
-        double linearAve = 0, doubleAve = 0, chainAve = 0;
+        double linearCollisionSum = 0, doubleCollisionSum = 0, chainCollisionSum = 0;
 
         // Initiate table
         Table<Integer, String> table = new Table<Integer, String>(241);
@@ -27,30 +27,58 @@ public class HashTesting {
         TableChainHash<Integer, String> chainTable = new TableChainHash<Integer, String>(241);
 
         // Run collision test
-        System.out.println("Attempt    linear    double    chain");
+        System.out.println();
+        System.out.println("Attempt    linear   double   chain");
         while (file.hasNextLine()) {
             count++;
             name = file.next();
             key = file.nextInt();
 
+            // Add data to table
             table.put(key, name);
             doubleTable.put(key, name);
             chainTable.put(key, name);
 
-            System.out.print(count + "             ");
-            System.out.print(table.getCollisionCount() + "            ");
-            System.out.print(doubleTable.getCollisionCount() + "            ");
-            System.out.println(chainTable.getCollisionCount() + "            ");
+            // Format ints into strings with the proper spacing
+            String countStr = formattedNumString(count);
+            String tableCollisionStr = formattedNumString(table.getCollisionCount());
+            String doubleTableCollisionStr = formattedNumString(doubleTable.getCollisionCount());
+            String chainTableCollisionStr = formattedNumString(chainTable.getCollisionCount());
 
-            linearAve += table.getCollisionCount();
-            doubleAve += doubleTable.getCollisionCount();
-            chainAve += chainTable.getCollisionCount();
+            // Print collision report on each input into table
+            System.out.print(countStr);
+            System.out.print(tableCollisionStr);
+            System.out.print(doubleTableCollisionStr);
+            System.out.println(chainTableCollisionStr);
+
+            // Add collision count
+            linearCollisionSum += table.getCollisionCount();
+            doubleCollisionSum += doubleTable.getCollisionCount();
+            chainCollisionSum += chainTable.getCollisionCount();
         }
 
         // Print result
+        // ...CollisionSum/count returns average since count will be 200 (the total number of elements)
         System.out.println();
-        System.out.println("Average for Linear: " + linearAve/200);
-        System.out.println("Average for Double: " + doubleAve/200);
-        System.out.println("Average for Chain: " + chainAve/200);
+        System.out.println("Average for Linear: " + linearCollisionSum/count);
+        System.out.println("Average for Double: " + doubleCollisionSum/count);
+        System.out.println("Average for Chain: " + chainCollisionSum/count);
+    }
+
+    // Formats integers [0-999] and returns them as a string with proper spacing for collision report printing
+    // Takes a parameter called num of type int that is to be formatted
+    private static String formattedNumString(int num) {
+        String numString = "";
+
+        if (num >= 0 && num < 10) {
+            numString = "  " + num + "      ";
+        } else if (num < 100) {
+            numString = " " + num + "      ";
+        } else {
+            numString = num + "      ";
+        }
+
+        return numString;
     }
 }
+
